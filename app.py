94535787f63dc45846f999dfb697ba574f7854bc
Rohini Tembhurnikar <98411216+rohinirt@users.noreply.github.com>
2026-09-07 22:35:57 +0530

    Fix airline selection to include all options

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ page = st.sidebar.radio("Navigation", ["Arrivals Intelligence", "Departures Inte
 # Sidebar Global Filters
 st.sidebar.markdown("---")
 st.sidebar.subheader("Filter Data")
-airlines = conn.execute('SELECT DISTINCT "AIRLINE_CODE" FROM flights WHERE "AIRLINE_CODE" IS NOT NULL').df()['AIRLINE_CODE'].tolist()[cite: 1]
+airlines = conn.execute('SELECT DISTINCT "AIRLINE_CODE" FROM flights WHERE "AIRLINE_CODE" IS NOT NULL').df()['AIRLINE_CODE'].tolist()
 selected_airline = st.sidebar.multiselect("Select Airline", options=airlines, default=[])
 
 airline_filter = ""
